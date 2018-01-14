fa6179f8e9268d995a49c3ee8a6f7bdd181487fc
masadeh <mohammad.masadeh@reserveout.com>
2018-01-14 17:02:14 +0200

    fixes

--- src/main/java/com/task/expedia/HotelController.java
+++ src/main/java/com/task/expedia/HotelController.java
@@ -23,27 +23,21 @@ public class HotelController {
 	private String expediaURI = "https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel";
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(Model model, @RequestParam(value = "destinationName", required = false) String destinationName,
-			@RequestParam(value = "minTripStartDate", required = false) String minTripStartDate,
+	public String getHotels(Model model, @RequestParam(value = "destinationName", required = false) String destinationName,
+			@RequestParam(value = "maxTotalRate", required = false) String maxTotalRate,
 			@RequestParam(value = "maxTripStartDate", required = false) String maxTripStartDate) {
-		LocalDate min = null;
-		LocalDate max = null;
 		RestTemplate restTemplate = new RestTemplate();
 		StringBuilder url = new StringBuilder();
 		url.append(expediaURI);
-		final DateTimeFormatter dtf = DateTimeFormat.forPattern("dd/mm/yyyy");
-
 		if (destinationName != null && !destinationName.equals("")) {
 			url.append("&destinationName=" + destinationName);
 		}
 
-		if (minTripStartDate != null && !minTripStartDate.equals("")) {
-			min = dtf.parseLocalDate(minTripStartDate);
-			url.append("&minTripStartDate=" + minTripStartDate);
+		if (maxTotalRate != null && !maxTotalRate.equals("")) {
+			url.append("&maxTotalRate=" + maxTotalRate);
 		}
 
 		if (maxTripStartDate != null && !maxTripStartDate.equals("")) {
-			max = dtf.parseLocalDate(maxTripStartDate);
 			url.append("&maxTripStartDate=" + maxTripStartDate);
 		}
 		HotelDeals hotels = restTemplate.getForObject(url.toString(), HotelDeals.class);
